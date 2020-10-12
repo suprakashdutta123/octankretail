9051bc64cdd016f84312f741771ae71657628325
Dutta <suprakd@amazon.com>
2020-10-12 17:38:39 -0500

    render successful

--- src/App.js
+++ src/App.js
@@ -4,37 +4,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: null,
+      items: [],
       isLoaded: false,
     };
   }
 
   componentDidMount() {
-    fetch("https://d3rno3xn80u5m0.cloudfront.net/dev/customer")
+    this.getCustomerData();
+  }
+
+  async getCustomerData() {
+    console.log("Calling API !!!");
+    fetch(
+      "https://dfiups7yk7.execute-api.us-east-2.amazonaws.com/dev/customer",
+      {
+        method: "get",
+      }
+    )
       .then((res) => res.json())
-      .then((data) => console.log(data));
-    // .then((json) => {
-    //    this.state({
-    //     isLoaded: true,
-    //      items: json,
-    //   });
-    //  });
+      // .then((data) => console.log(data))
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
   }
 
   render() {
-    return <div>Is Loading !!!!!!!!!....</div>;
-  }
-  /*
     var { isLoaded, items } = this.state;
+
+    console.log(isLoaded, items);
 
     if (!isLoaded) {
       return <div>Is Loading !!!!!!!!!....</div>;
     } else {
-      return <div classname="App">Data has been loaded</div>;
+      return (
+        <div className="App">
+          Data has been loaded{" "}
+          {this.state.items.map((customer) => (
+            <div key={customer.customerID}>
+              <h3>{customer.customerFirstName}</h3>
+            </div>
+          ))}
+        </div>
+      );
     }
   }
-
-  */
 }
 
 export default App;
